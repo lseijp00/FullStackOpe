8a27e87a1bd58d106ca143baa3185a4a430635c5
lseijp00 <lseijpp00@estudiantes.unileon.es>
2022-05-07 13:54:53 +0200

    anecdotes, 1.14

--- part 1/anecdotes/src/App.jsx
+++ part 1/anecdotes/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 export default function App(props) {
   const [selected, setSelected] = useState(0)
+  const [quoteMostVote, setQuoteMostVote] = useState(0)
+
   const [copy, setCopy] = useState(Array(props.anecdotes.length).fill(0))
 
   const handleRandom = () => {
@@ -15,12 +17,23 @@ export default function App(props) {
     setCopy(poinstCopy)
   }
 
+  useEffect(() => {
+    const max = Math.max(...copy)
+    const index = copy.indexOf(max)
+    setQuoteMostVote(props.anecdotes[index])
+  }, [copy])
+
   return (
-    <div className='wrapper'>
-      {<h4>{props.anecdotes[selected]}</h4>}
-      <button onClick={handleRandom}>next anecdote</button>
-      <button onClick={handleVote}>vote</button>
-      <p>It has {copy[selected]} votes</p>
-    </div>
+    <>
+      <div className='wrapper'>
+        <h2>Anecdote of the day</h2>
+        {<h4>{props.anecdotes[selected]}</h4>}
+        <button onClick={handleRandom}>next anecdote</button>
+        <button onClick={handleVote}>vote</button>
+        <p>It has {copy[selected]} votes</p>
+      </div>
+      <h2>Anecdote with most votes</h2>
+      <p>{quoteMostVote}</p>
+    </>
   )
 }

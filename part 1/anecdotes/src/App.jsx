8b57c017c50f8935b0c4e431a65f687ddc5f89af
lseijp00 <lseijpp00@estudiantes.unileon.es>
2022-05-07 13:46:14 +0200

    anecdotes, 1.13

--- part 1/anecdotes/src/App.jsx
+++ part 1/anecdotes/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 
-export default function App (props) {
+export default function App(props) {
   const [selected, setSelected] = useState(0)
+  const [copy, setCopy] = useState(Array(props.anecdotes.length).fill(0))
 
   const handleRandom = () => {
     setSelected(Math.floor(Math.random() * (props.anecdotes.length - 0)))
   }
+
+  const handleVote = () => {
+    const poinstCopy = [...copy]
+    poinstCopy[selected] += 1
+    setCopy(poinstCopy)
+  }
+
   return (
     <div className='wrapper'>
-      {props.anecdotes[selected]}
+      {<h4>{props.anecdotes[selected]}</h4>}
       <button onClick={handleRandom}>next anecdote</button>
-
+      <button onClick={handleVote}>vote</button>
+      <p>It has {copy[selected]} votes</p>
     </div>
   )
 }

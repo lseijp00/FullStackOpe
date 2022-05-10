e3e131322eba9fdc5224e79a2c71356d41efce4a
lseijp00 <lseijpp00@estudiantes.unileon.es>
2022-05-10 14:52:51 +0200

    Phonebook, 2.8

--- part 2/Phonebook/src/App.jsx
+++ part 2/Phonebook/src/App.jsx
@@ -1,34 +1,54 @@
 import { useState } from 'react'
 import './App.css'
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '987456382' },
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleNoteChange = (e) => {
+  const handleNameChange = (e) => {
     e.preventDefault()
     setNewName(e.target.value)
   }
+  const handleNumberChange = (e) => {
+    e.preventDefault()
+    setNewNumber(e.target.value)
+  }
+
   const addContact = (e) => {
     e.preventDefault()
-    const newContact = { name: newName }
+    const newContact = { name: newName, number: newNumber }
     const arrNames = persons.map((person) => person.name)
     if (arrNames.includes(newContact.name))
       alert(`${newName} already exists in array`)
     else setPersons([...persons, newContact])
 
     setNewName('')
+    setNewNumber('')
   }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addContact}>
-        <input value={newName} onChange={handleNoteChange} />
+      <form
+        style={{
+          display: 'inline-flex',
+          gap: '.5rem',
+          flexDirection: 'column',
+        }}
+        onSubmit={addContact}
+      >
+        name <input value={newName} onChange={handleNameChange} />
+        number <input value={newNumber} onChange={handleNumberChange} />
         <button type='submit'>add</button>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, i) => (
-          <li key={i}>{person.name}</li>
+          <li key={i}>
+            {person.name} --- {person.number}
+          </li>
         ))}
       </ul>
     </div>

d2f6054f534ba852ecb487102f0e2043a6878432
lseijp00 <lseijpp00@estudiantes.unileon.es>
2022-05-10 14:21:37 +0200

    Phonebook, 2.6

---
+++ part 2/Phonebook/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const handleNoteChange = (e) => {
+    e.preventDefault()
+    setNewName(e.target.value)
+  }
+  const addContact = (e) => {
+    e.preventDefault()
+    const newContact = { name: newName }
+    setPersons([...persons, newContact])
+    setNewName('')
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <input value={newName} onChange={handleNoteChange} />
+        <button type='submit'>add</button>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, i) => (
+          <li key={i}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App

abc5d01d2ded94d2e71ee56ec1219ad97fdbbeb1
lseijp00 <lseijpp00@estudiantes.unileon.es>
2022-05-10 19:12:08 +0200

    Phonebook, 2.9

--- part 2/Phonebook/src/App.jsx
+++ part 2/Phonebook/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 import './App.css'
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '987456382' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [personsFiltered, setPersonsFiltered] = useState([])
 
   const handleNameChange = (e) => {
     e.preventDefault()
@@ -15,7 +20,12 @@ const App = () => {
     e.preventDefault()
     setNewNumber(e.target.value)
   }
-
+  const handleFilter = (e) => {
+    e.preventDefault()
+    const arr = persons.filter((person) => person.name.includes(e.target.value))
+    setPersonsFiltered(arr)
+    setFilter(e.target.value)
+  }
   const addContact = (e) => {
     e.preventDefault()
     const newContact = { name: newName, number: newNumber }
@@ -31,6 +41,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <p>filter shown with a </p>
+        <input onChange={handleFilter} />
+      </div>
+      <h3>Add a new</h3>
       <form
         style={{
           display: 'inline-flex',
@@ -45,11 +60,21 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, i) => (
-          <li key={i}>
-            {person.name} --- {person.number}
-          </li>
-        ))}
+        {personsFiltered.length === 0 && filter.length > 0 ? (
+          <div>No coincide nada</div>
+        ) : personsFiltered.length > 0 && filter.length > 0 ? (
+          personsFiltered.map((person, i) => (
+            <li key={i}>
+              {person.name} --- {person.number}
+            </li>
+          ))
+        ) : (
+          persons.map((person, i) => (
+            <li key={i}>
+              {person.name} --- {person.number}
+            </li>
+          ))
+        )}
       </ul>
     </div>
   )

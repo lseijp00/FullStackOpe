ca502295f4731bc9bc79480150de2e213a751327
lseijp00 <lseijpp00@estudiantes.unileon.es>
2022-05-10 22:42:10 +0200

    DataCountries, 2.13

--- part 2/DataCountries/src/App.jsx
+++ part 2/DataCountries/src/App.jsx
@@ -5,6 +5,8 @@ import axios from 'axios'
 function App() {
   const [newCountry, setNewCountry] = useState('')
   const [countries, setCountries] = useState([])
+  const [showCountry, setShowCountry] = useState(false)
+  const [countryDetails, setShowCountryDetails] = useState([])
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all').then((response) => {
@@ -19,36 +21,57 @@ function App() {
     e.preventDefault()
     setNewCountry(e.target.value)
   }
+
+  const showDetails = (country) => {
+    setShowCountry(true)
+    setShowCountryDetails(country)
+  }
+
   console.log(countries)
   return (
     <div className='App'>
-      <label>Find Countries</label>
-      <input value={newCountry} type='text' onChange={handleChange} />
-      <section>
-        <h3>Countries</h3>
-        {countries.length < 10 && countries.length > 1 ? (
-          countries.map((country) => (
-            <p key={country.name.common}>{country.name.common}</p>
-          ))
-        ) : countries.length === 1 ? (
-          countries.map((country) => (
-            <div key={country}>
-              <h3>{country.name.common}</h3>
-              <p>capital {country.capital}</p>
-              <p>area {country.area}</p>
+      <div>
+        <label>Find Countries</label>
+        <input value={newCountry} type='text' onChange={handleChange} />
+      </div>
+      <div className='wrapped-sections'>
+        <section>
+          <h3>Countries</h3>
+          {countries.length < 10 && countries.length >= 1 ? (
+            countries.map((country) => (
+              <div key={country.name.common} className='country'>
+                <p key={country.name.common}>{country.name.common}</p>
+                <button
+                  className='country__button'
+                  onClick={() => showDetails(country)}
+                >
+                  show
+                </button>
+              </div>
+            ))
+          ) : (
+            <p>No countries found</p>
+          )}
+        </section>
+        <section>
+          {showCountry ? (
+            <div>
+              <h3>{countryDetails.name.common}</h3>
+              <p>capital {countryDetails.capital}</p>
+              <p>area {countryDetails.area}</p>
               <h5>languages:</h5>
               <ul>
-                {Object.values(country.languages).map((language) => (
+                {Object.values(countryDetails.languages).map((language) => (
                   <li key={language}>{language}</li>
                 ))}
               </ul>
-              <img src={country.flags.svg} alt='' />
+              <img src={countryDetails.flags.svg} alt='' />
             </div>
-          ))
-        ) : (
-          <p>No countries found</p>
-        )}
-      </section>
+          ) : (
+            <p>No se han clicado detalles</p>
+          )}
+        </section>
+      </div>
     </div>
   )
 }
